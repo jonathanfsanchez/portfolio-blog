61035deae5243ccf74794c2035576a53ee40929f
Jonathan Sanchez <sanchez.jonathan@gmail.com>
2020-09-03 11:51:19 -0700

    Small Changes to Resume Page

--- portfolioblog/settings.py
+++ portfolioblog/settings.py
@@ -25,8 +25,7 @@ SECRET_KEY = 'SOME_KEY'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ["*"]
 
 # Application definition
 
