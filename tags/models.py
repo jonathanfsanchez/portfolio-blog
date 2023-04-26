5f1fa2307421fdcd23dd9227a2bee1b0275d363e
Jonathan Sanchez <sanchez.jonathan@gmail.com>
2023-04-25 18:58:27 -0700

    fix tags slug - unique

--- tags/models.py
+++ tags/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Tag(models.Model):
     displayable_name = models.TextField()
-    name_slug = models.SlugField()
+    name_slug = models.SlugField(unique=True)
     parent_tag = models.ForeignKey('Tag', null=True, blank=True, on_delete=models.DO_NOTHING)
 
     def __str__(self):
